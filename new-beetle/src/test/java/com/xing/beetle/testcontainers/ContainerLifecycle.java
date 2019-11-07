789f47d230911a0204a285cd557ba92e5c2a4ed5
Sven Linstaedt <sven.linstaedt@gmail.com>
2019-11-07 18:18:57 +0100

    parallelize container start

--- new-beetle/src/test/java/com/xing/beetle/testcontainers/ContainerLifecycle.java
+++ new-beetle/src/test/java/com/xing/beetle/testcontainers/ContainerLifecycle.java
@@ -1,10 +1,12 @@
 package com.xing.beetle.testcontainers;
 
+import static java.util.concurrent.CompletableFuture.runAsync;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
-
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.InvocationInterceptor;
 import org.junit.jupiter.api.extension.ReflectiveInvocationContext;
@@ -12,13 +14,13 @@ import org.testcontainers.lifecycle.Startable;
 
 public class ContainerLifecycle implements InvocationInterceptor {
 
-  private static Stream<Startable> containers(ReflectiveInvocationContext<?> context) {
-    return context
-        .getArguments()
-        .stream()
-        .flatMap(ContainerLifecycle::flatten)
-        .filter(Startable.class::isInstance)
-        .map(Startable.class::cast);
+  private static void doWithContainers(ReflectiveInvocationContext<?> context,
+      Consumer<Startable> action) {
+    CompletableFuture<?>[] futures =
+        context.getArguments().stream().flatMap(ContainerLifecycle::flatten)
+            .filter(Startable.class::isInstance).map(Startable.class::cast)
+            .map(s -> runAsync(s::start)).toArray(CompletableFuture<?>[]::new);
+    CompletableFuture.allOf(futures).join();
   }
 
   private static Stream<?> flatten(Object o) {
@@ -35,28 +37,24 @@ public class ContainerLifecycle implements InvocationInterceptor {
 
   private static void lifecycled(Invocation<?> invocation, ReflectiveInvocationContext<?> context)
       throws Throwable {
-    containers(context).forEach(Startable::start);
+    doWithContainers(context, Startable::start);
     try {
       invocation.proceed();
     } finally {
-      containers(context).forEach(Startable::stop);
+      doWithContainers(context, Startable::stop);
     }
   }
 
   @Override
-  public void interceptTestMethod(
-      Invocation<Void> invocation,
-      ReflectiveInvocationContext<Method> invocationContext,
-      ExtensionContext extensionContext)
+  public void interceptTestMethod(Invocation<Void> invocation,
+      ReflectiveInvocationContext<Method> invocationContext, ExtensionContext extensionContext)
       throws Throwable {
     lifecycled(invocation, invocationContext);
   }
 
   @Override
-  public void interceptTestTemplateMethod(
-      Invocation<Void> invocation,
-      ReflectiveInvocationContext<Method> invocationContext,
-      ExtensionContext extensionContext)
+  public void interceptTestTemplateMethod(Invocation<Void> invocation,
+      ReflectiveInvocationContext<Method> invocationContext, ExtensionContext extensionContext)
       throws Throwable {
     lifecycled(invocation, invocationContext);
   }
