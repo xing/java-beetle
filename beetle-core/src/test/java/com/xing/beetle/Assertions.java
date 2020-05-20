27a3d57825507e028d2f0e6645831ff033c0fd36
Jens Hausherr <jens.hausherr@xing.com>
2020-05-20 16:10:46 +0200

    Replace fixed sleep by wait

---
+++ beetle-core/src/test/java/com/xing/beetle/Assertions.java
@@ -0,0 +1,42 @@
+package com.xing.beetle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class Assertions {
+
+  public static <T> void assertEventualLength(List<T> a, int len, long timeout) throws Exception {
+    CountDownLatch l = new CountDownLatch(1);
+
+    new Thread(
+            new Runnable() {
+              @Override
+              public void run() {
+                for (; ; ) {
+                  if (a.size() == len) {
+                    l.countDown();
+                  }
+                  try {
+                    if (l.await(5, TimeUnit.MILLISECONDS)) {
+                      return;
+                    }
+                  } catch (InterruptedException e) {
+                    return;
+                  }
+                }
+              }
+            })
+        .run();
+
+    if (l.await(timeout, TimeUnit.MILLISECONDS)) {
+      assertEquals(a.size(), len);
+      return;
+    }
+
+    l.countDown();
+    assertEquals(a.size(), len);
+  }
+}
