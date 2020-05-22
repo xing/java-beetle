b85ccb6d800c2ca17f3cd912a11782969f5c715b
Jens Hausherr <jens.hausherr@xing.com>
2020-05-22 11:24:06 +0200

    Add SVUID

--- redis-dedup-store/src/main/java/com/xing/beetle/redis/DeduplicationException.java
+++ redis-dedup-store/src/main/java/com/xing/beetle/redis/DeduplicationException.java
@@ -2,6 +2,11 @@ package com.xing.beetle.redis;
 
 class DeduplicationException extends RuntimeException {
 
+  /**
+   *
+   */
+  private static final long serialVersionUID = 1L;
+
   DeduplicationException(String message, Throwable e) {
     super(message, e);
   }
