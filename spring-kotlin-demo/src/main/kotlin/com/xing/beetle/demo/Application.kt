337b1e3deff3ab349b0664a7e42c96884b831472
Pin-Sho Feng <ps-feng@users.noreply.github.com>
2020-05-22 09:19:28 +0200

    Update Kotlin sample (#3)
    Updated with a more idiomatic and annotation-free approach.

--- spring-kotlin-demo/src/main/kotlin/com/xing/beetle/demo/Application.kt
+++ spring-kotlin-demo/src/main/kotlin/com/xing/beetle/demo/Application.kt
@@ -3,39 +3,43 @@ package com.xing.beetle.demo
 import org.springframework.amqp.core.AmqpTemplate
 import org.springframework.amqp.core.Message
 import org.springframework.amqp.core.MessageProperties
-import org.springframework.amqp.rabbit.annotation.Exchange
-import org.springframework.amqp.rabbit.annotation.Queue
-import org.springframework.amqp.rabbit.annotation.QueueBinding
-import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.ExitCodeGenerator
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationContext
+import org.springframework.stereotype.Component
 import java.util.*
 
-@SpringBootApplication
-class Application {
-    @RabbitListener(bindings = [QueueBinding(value = Queue(value = "myQueue", durable = "false"),
-            exchange = Exchange(value = "auto.exch", ignoreDeclarationExceptions = "true"), key = ["orderRoutingKey"])], ackMode = "AUTO")
-    fun processOrder(message: Message?) {
-        println(message)
-    }
+fun main() {
+    runApplication<Application>()
+}
 
-    companion object {
-        @Throws(InterruptedException::class)
-        @JvmStatic
-        fun main(args: Array<String>) {
-            val context = SpringApplication.run(Application::class.java, *args)
-            val template = context.getBean(AmqpTemplate::class.java)
-            val props = MessageProperties()
-            props.messageId = UUID.randomUUID().toString()
-            val message = Message(ByteArray(0), props)
-            for (i in 0..39) {
-                template.send("myQueue", message)
-            }
-            for (i in 0..3) {
-                template.convertAndSend("myQueue", "hello world")
-            }
-            Thread.sleep(2000)
-            context.close()
+@SpringBootApplication
+class Application
+
+@Component
+class AmqpDemo(
+    private val appContext: ApplicationContext,
+    private val template: AmqpTemplate
+) : CommandLineRunner {
+    override fun run(vararg args: String) {
+        val message = Message(
+            ByteArray(0),
+            MessageProperties().apply { messageId = UUID.randomUUID().toString() }
+        )
+
+        repeat(40) {
+            template.send("myQueue", message)
         }
+        repeat(4) {
+            template.convertAndSend("myQueue", "hello world")
+        }
+        repeat(44) {
+            println(template.receive("myQueue", 50))
+        }
+
+        SpringApplication.exit(appContext, ExitCodeGenerator { 0 })
     }
 }
