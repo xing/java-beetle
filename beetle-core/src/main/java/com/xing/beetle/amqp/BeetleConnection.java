48571985c6e2353be9cfd9c0b0175b4b9b9ea5ba
Jens Hausherr <jens.hausherr@xing.com>
2020-05-28 17:01:58 +0200

    Extend documentation

--- beetle-core/src/main/java/com/xing/beetle/amqp/BeetleConnection.java
+++ beetle-core/src/main/java/com/xing/beetle/amqp/BeetleConnection.java
@@ -2,19 +2,25 @@ package com.xing.beetle.amqp;
 
 import static java.util.Objects.requireNonNull;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ShutdownListener;
-import com.rabbitmq.client.ShutdownSignalException;
-import com.xing.beetle.util.ExceptionSupport.Function;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ShutdownListener;
+import com.rabbitmq.client.ShutdownSignalException;
+import com.xing.beetle.util.ExceptionSupport.Function;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * BeetleConnection aggregates connections to one or more brokers to one virtual
+ * connection.
+ */
 public class BeetleConnection implements DefaultConnection.Decorator, ShutdownListener {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(BeetleConnection.class);
@@ -22,6 +28,11 @@ public class BeetleConnection implements DefaultConnection.Decorator, ShutdownLi
   private final List<? extends Connection> delegates;
   private final Set<ShutdownListener> shutdownListeners;
 
+  /**
+   * Build a new BeetleConnection that aggregfates the given connections.
+   *
+   * @param connections Wrapped AMQP connections.
+   */
   public BeetleConnection(List<Connection> connections) {
     this.delegates = new ArrayList<>(connections);
     this.shutdownListeners = new HashSet<>();
@@ -37,10 +48,7 @@ public class BeetleConnection implements DefaultConnection.Decorator, ShutdownLi
   public Channel createChannel(int channelNumber) throws IOException {
     List<Channel> channels = new ArrayList<>();
     for (Connection connection : delegates) {
-      channels.add(
-          channelNumber >= 0
-              ? connection.createChannel(channelNumber)
-              : connection.createChannel());
+      channels.add(channelNumber >= 0 ? connection.createChannel(channelNumber) : connection.createChannel());
     }
     return new BeetleChannel(channels);
   }
@@ -63,18 +71,12 @@ public class BeetleConnection implements DefaultConnection.Decorator, ShutdownLi
       if (cause.isInitiatedByApplication()) {
         LOGGER.debug("Connection {} closed because of {}", connection, cause.getReason());
       } else {
-        LOGGER.warn(
-            "Connection to the broker at {}:{} lost, reconnecting in 10 seconds. Reason: {}",
-            connection.getAddress(),
-            connection.getPort(),
-            cause.getReason());
+        LOGGER.warn("Connection to the broker at {}:{} lost, reconnecting in 10 seconds. Reason: {}",
+            connection.getAddress(), connection.getPort(), cause.getReason());
       }
     } else {
       Channel channel = (Channel) cause.getReference();
-      LOGGER.info(
-          "AQMP channel shutdown {} because of {}. Doing nothing about it.",
-          channel,
-          cause.getReason());
+      LOGGER.info("AQMP channel shutdown {} because of {}. Doing nothing about it.", channel, cause.getReason());
     }
   }
 }
