1090d9513b7cc2d381dd808c6e1dd46a594332a6
hakki.bagci <hakki.bagci@new-work.se>
2020-06-04 13:41:39 +0200

    use unit timestamp for expires_at.

--- beetle-core/src/main/java/com/xing/beetle/BeetleHeader.java
+++ beetle-core/src/main/java/com/xing/beetle/BeetleHeader.java
@@ -2,5 +2,6 @@ package com.xing.beetle;
 
 public interface BeetleHeader {
   String PUBLISH_REDUNDANCY = "x-publish-message-redundancy";
+  // unix timestamp after which the message should be considered stale (seconds)
   String EXPIRES_AT = "expires_at";
 }
