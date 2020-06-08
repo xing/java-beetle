2c96c692674537baaffde20c01ce78202697b6f2
hakki.bagci <hakki.bagci@new-work.se>
2020-06-08 16:48:30 +0200

    add a app that demonstrates use of beetle client without Spring.

--- beetle-core/src/test/java/com/xing/beetle/NewBeetleAcceptanceBeetleIT.java
+++ beetle-core/src/test/java/com/xing/beetle/NewBeetleAcceptanceBeetleIT.java
@@ -1,21 +1,22 @@
 package com.xing.beetle;
 
-import static com.xing.beetle.Assertions.assertEventualLength;
-import static org.mockito.Mockito.when;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Delivery;
 import com.xing.beetle.amqp.BeetleAmqpConfiguration;
 import com.xing.beetle.amqp.BeetleConnectionFactory;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mockito;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.xing.beetle.Assertions.assertEventualLength;
+import static org.mockito.Mockito.when;
 
 @Testcontainers
 class NewBeetleAcceptanceBeetleIT extends BaseBeetleIT {
@@ -23,8 +24,10 @@ class NewBeetleAcceptanceBeetleIT extends BaseBeetleIT {
   @ParameterizedTest(name = "Brokers={0}")
   @ValueSource(ints = {1, 2})
   void testRedundantPublishWithoutDeduplication(int containers) throws Exception {
-    BeetleConnectionFactory factory = new BeetleConnectionFactory(beetleAmqpConfiguration());
-    Connection connection = createConnection(factory, containers);
+    BeetleConnectionFactory factory =
+        new BeetleConnectionFactory(beetleAmqpConfiguration(containers));
+
+    Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
     String queue = String.format("nodedup-%d", containers);
 
@@ -42,16 +45,22 @@ class NewBeetleAcceptanceBeetleIT extends BaseBeetleIT {
     assertEventualLength(messages, Math.min(containers, 2), 500);
   }
 
-  BeetleAmqpConfiguration beetleAmqpConfiguration() {
+  BeetleAmqpConfiguration beetleAmqpConfiguration(int containers) {
+
+    List<String> rabbitAddresses =
+        Arrays.stream(rmq)
+            .map(rabbit -> rabbit.getContainerIpAddress() + ":" + rabbit.getFirstMappedPort())
+            .limit(containers)
+            .collect(Collectors.toList());
+
     BeetleAmqpConfiguration beetleAmqpConfiguration = Mockito.mock(BeetleAmqpConfiguration.class);
 
+    when(beetleAmqpConfiguration.getBeetleServers()).thenReturn(String.join(",", rabbitAddresses));
     when(beetleAmqpConfiguration.getBeetlePolicyExchangeName()).thenReturn("beetle-policies");
     when(beetleAmqpConfiguration.getBeetlePolicyUpdatesQueueName())
         .thenReturn("beetle-policy-updates");
     when(beetleAmqpConfiguration.getBeetlePolicyUpdatesRoutingKey())
         .thenReturn("beetle.policy.update");
-
-    when(beetleAmqpConfiguration.getBeetleServers()).thenReturn("");
 
     return beetleAmqpConfiguration;
   }
